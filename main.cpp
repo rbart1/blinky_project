9f7554c6b876247cff84e929fc02a52f378a618c
DUVERDIER Romain <rduverdier@caisse.pedago.ipb.fr>
2025-01-21 11:51:15 +0100

    interrupt_du_btn

--- main.cpp
+++ main.cpp
@@ -7,36 +7,35 @@
 
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     200ms
+#define BLINKING_RATE     250ms
 
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
+// Initialise the digital pin LED1 as an output
 
 #ifdef LED1
     DigitalOut led(LED1);
 #else
     bool led;
-
 #endif
 
 #ifdef BUTTON1
-    DigitalIn button(BUTTON1);
+    InterruptIn button(BUTTON1);
 #else
     bool button;
-
 #endif
 
+void toggle_led()
+{
+    led = !led;
+}
+
+int main()
+{
+    button.fall(&toggle_led);
+    button.rise(&toggle_led);
+
     while (true) {
-        if(button.read() == 1){
-            led = 1 ;
-            printf("BTN1_ON\n");
-        }
-        else{
-            led = 0;
-            printf("BTN1_OFF\n");
-        }
+        printf("Alive!\n");
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
