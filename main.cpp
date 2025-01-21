80af8e00a583db37dfe7f2725615c13a1423dfba
DUVERDIER Romain <rduverdier@caisse.pedago.ipb.fr>
2025-01-21 11:12:58 +0100

    Polling_du_btn

--- main.cpp
+++ main.cpp
@@ -7,21 +7,36 @@
 
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+#define BLINKING_RATE     200ms
 
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
+
 #ifdef LED1
     DigitalOut led(LED1);
 #else
     bool led;
+
+#endif
+
+#ifdef BUTTON1
+    DigitalIn button(BUTTON1);
+#else
+    bool button;
+
 #endif
 
     while (true) {
-        led = !led;
-	    printf("test\n");
+        if(button.read() == 1){
+            led = 1 ;
+            printf("BTN1_ON\n");
+        }
+        else{
+            led = 0;
+            printf("BTN1_OFF\n");
+        }
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
